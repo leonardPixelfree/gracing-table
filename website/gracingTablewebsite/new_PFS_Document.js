2af3a586470aad39fdfecc62948fa95445035c9b
leonard <leonard@pixelfreestudio.com>
2023-09-14 19:56:12 +0200

    initial commit

---
+++ website/gracingTablewebsite/new_PFS_Document.js
@@ -0,0 +1,8 @@
+var paragraphParent = document.getElementById("paragraph2").parentElement;
+if (paragraphParent.id.endsWith("container")) {
+	var parentRegion = document.getElementById(paragraphParent.id).parentElement;
+	parentRegion.style.webkitTextFillColor = "#ffffff";
+	paragraphParent.style.webkitTextFillColor = "#ffffff";
+}
+
+
